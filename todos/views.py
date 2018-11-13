60853582bb2e10b75e58f90b41725068a255b384
Piotr Roksela <piotr.roksela@boldare.com>
2018-11-13 11:52:29 +0100

    todos basic

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
+from todos.models import Todo
 
 
 class TodosIndex(TemplateView):
     template_name = 'todos.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(TodosIndex, self).get_context_data(**kwargs)
+        context['todos'] = Todo.objects.all()
+        return context
