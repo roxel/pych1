1f81a795076d0019d5da8e1e781c77cf3aacf06a
Piotr Roksela <piotr.roksela@boldare.com>
2018-11-21 13:26:50 +0100

    places - more advanced models

--- places/admin.py
+++ places/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin, gis
 
 
-from places.models import Place, Category
+from places.models import Place, Category, Rating
 
-admin.site.register(Place, gis.admin.OSMGeoAdmin)
+
+class RatingInlineAdmin(admin.TabularInline):
+    model = Rating
+    fields = ('value', 'comment', 'user')
+
+
+class PlaceAdmin(gis.admin.OSMGeoAdmin):
+    inlines = [RatingInlineAdmin]
+    fields = ('name', 'established', 'description', 'categories', 'location')
+
+
+admin.site.register(Place, PlaceAdmin)
 admin.site.register(Category)
--- places/models.py
+++ places/models.py
@@ -1,4 +1,7 @@
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.contrib.gis.geos import Point
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.contrib.gis.db.models import PointField
 
@@ -17,9 +20,22 @@ class Category(models.Model):
 
 class Place(models.Model):
     name = models.CharField(max_length=500)
+    established = models.DateField(null=True, blank=True)
     location = PointField(default=Point(x=18.662252, y=50.291149))
     description = models.TextField(blank=True)
     categories = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.name
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    value = models.IntegerField(validators=(MinValueValidator(0), MaxValueValidator(5)))
+    comment = models.TextField(blank=True)
+    added = models.DateField(auto_now_add=True)
+    edited = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.value}/5"
